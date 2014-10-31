a7ff716920a90f66176a5d10390c5fa79b888169
lbx6z <tcluzhe@hotmail.com>
2014-10-31 13:47:34 +0800

    memory buf code

---
+++ DBMS/DBMS/node.cpp
@@ -0,0 +1,54 @@
+#include "node.h"
+#include <fstream>
+
+void writePageToFile(int pageid, char* data, char* filename){}
+
+FileBuffer::FileBuffer(int max){
+	header = new Node();
+	trailor = new Node();
+	header -> next = trailor;
+	trailor -> pre = header;
+	
+	maxpagenum = max;
+	usepagenum = 0;
+}
+
+
+Node* FileBuffer::remove(){
+	if (usepagenum <= 0) return NULL;
+
+	Node* cur = trailor -> pre;
+	(cur -> pre) -> next = trailor;
+	trailor -> pre = cur -> pre;
+
+	if (cur -> dirty) writePageToFile(cur -> pageid, cur -> data, filename);
+
+	bufmap.erase(cur -> pageid);
+	usepagenum --;
+
+	return cur;
+}
+
+void FileBuffer::insert(Node* cur){
+	if (usepagenum == maxpagenum) {
+		cout << "buffer is full" << endl;
+		return;
+	}
+
+	cur -> next = header -> next;
+	cur -> pre = header;
+	header -> next = cur;
+	(cur -> next) -> pre = cur;
+
+	bufmap.insert(map<int, Node*>::value_type(cur -> pageid, cur));
+
+	usepagenum ++;
+}
+
+Node* FileBuffer::find(int pageid){
+	Node* cur = NULL;
+	map<int, Node*>::iterator it = bufmap.find(pageid);
+	if(it != bufmap.end())
+		cur = it ->second;
+	return cur;
+}
