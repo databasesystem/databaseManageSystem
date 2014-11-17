2ac14cafa93b9080bf6e048fe41127db4541d7a6
Alexia23 <wang1160997741@163.com>
2014-11-17 23:57:42 +0800

    finish B+tree

--- DBMS/DBMS/main.cpp
+++ DBMS/DBMS/main.cpp
@@ -1,9 +1,11 @@
 #include "node.h"
 #include "fileManage.h"
 #include "globalVariable.h"
+#include "BPlusTree.h"
 
 int main()
 {
+	// test buffer 
 	FileBuffer* fb = new FileBuffer(12);
 	Node *p1 = new Node("aaa", 1), *p2 = new Node("bbb", 2);
 	fb -> insert(p1);
@@ -14,6 +16,7 @@ int main()
 	p3 = fb -> find(1);
 	if (!p3) cout << "null" << endl;
 
+	// test file read or write by page
 	char data[PAGE_SIZE];
 	for (int i = 0; i < PAGE_SIZE-1; i++) 
 	{
@@ -36,6 +39,19 @@ int main()
 	char result[PAGE_SIZE];
 	FileManage::readPageFromFile(0,result, "hello.txt");
 	cout << result << endl;
+
+	//test B+ tree index
+	int agedata[10];
+	node * root;
+	root = NULL;
+	BPlusTree test;
+	for(int i = 0; i < 10; i++) 
+	{
+		root = test.insert_node(root,i, i);
+		test.print_tree(root);
+	}
+	test.find_and_print(root, 4, true);
+	
 	system("pause");
 	return 0;
 }
