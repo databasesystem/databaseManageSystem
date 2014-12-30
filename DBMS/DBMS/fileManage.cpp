5bcdb6851aabadf5996edac149a006f0718a7329
gzwongkk <gzwongkk@gmail.com>
2014-12-30 13:59:36 +0800

    Update fileManage.cpp

--- DBMS/DBMS/fileManage.cpp
+++ DBMS/DBMS/fileManage.cpp
@@ -1,5 +1,4 @@
 #include "fileManage.h"
-#include "globalVariable.h"
 #include "data_utility.h"
 #include <iostream>
 #include <stdio.h>
@@ -48,11 +47,7 @@ void FileManage::writePageToFile(int pageid, dbPage* pagedata, char* filename){
 }
 
 void FileManage::readPageFromFile(int pageid, dbPage* pageinfo, char* filename){
-<<<<<<< HEAD
 	//cout << "pageid: " << pageid << endl;
-=======
-	cout << "pageid: " << pageid << endl;
->>>>>>> 7c7512f5d8296b58c19eda3597a48b02fff09c79
 	FILE* filestream;
 	char* data = new char[sizeof(dbPage)];
 	filestream = fopen(filename,"r");
