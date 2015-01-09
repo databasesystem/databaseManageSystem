ed218675b41538024030147333d472dfc203d986
Alexia23 <wang1160997741@163.com>
2015-01-09 12:21:38 +0800

    delete parser unfinished

--- DBMS/DBMS/others.sql
+++ DBMS/DBMS/others.sql
@@ -10,4 +10,11 @@ name varchar(25) NOT NULL,
 rank int(10) NOT NULL,
 PRIMARY KEY (id)
 );
+INSERT INTO orders VALUES
+ (304403,209743,2);
+DELETE FROM publisher WHERE nation=’USA’;
+
+UPDATE book SET title=’Nine Times Nine’ WHERE authors=’Anthony Boucher’;
+SELECT title FROM book WHERE authors is null;
+SELECT book.title,orders.quantity FROM book,orders WHERE book.id=orders.book_id AND orders.quantity>8;
 
